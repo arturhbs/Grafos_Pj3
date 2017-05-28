bbe2089e2c3821f1c577eadf75a9d5fb25f00f6d
arturhbs <arturhenrique71@gmail.com>
2017-05-28 11:56:38 -0300

    Update projeto3.cpp

--- projeto3.cpp
+++ projeto3.cpp
@@ -24,23 +24,25 @@ typedef struct
 {
 	int cod_professor;
 	int habilitacoes;
-	vector<int> escolas_prioridade; 
+	vector<int> escolas_prioridade;
+	int flag_professor; 
+
 }t_professor;
 
 typedef struct 
 {	
-	string cod_escola;
+	int cod_escola;
 	int habilitacoes_pretendidas;
-	vector<string> professores_possiveis;
+	vector<int> professores_possiveis;
+	int flag_escola;
 
 }t_escola;
 
 /*VARIAVEIS GLOBAIS =====================================================*/
 t_professor professor[101];
 t_escola escola[51];
-/*=======================================================================*/
-
-void leitura_arquivo(){
+/*=======================================================================*/ 
+void Fazer_grafo(){
 	int j=0 , escola_leitura, i=0;
 	string line;
 	ifstream fp("arquivo.txt");
@@ -52,20 +54,76 @@ void leitura_arquivo(){
 			is >> aux >>  professor[i].cod_professor >> aux >> professor[i].habilitacoes >> aux;		
 			while(is >> escola_leitura != NULL ){
 				professor[i].escolas_prioridade.push_back(escola_leitura);/*coloca em um vetor a lista de prioridades das escolas*/
-				cout << "escola = " << escola_leitura << endl;		
 			}
-			cout << "cod_professor == "<< professor[i].cod_professor <<endl << "habilitacoes == " <<  professor[i].habilitacoes<<endl;
 			i++;
-			cout << " -------------" << endl;
 		}
 		else{
 			is >> aux >> escola[j].cod_escola >> aux >> escola[j].habilitacoes_pretendidas;
-			cout << "cod_escola = " <<  escola[j].cod_escola<< endl << "habilitacoes_pretendidas =  " << escola[j].habilitacoes_pretendidas << endl << "============" << endl; 
+			switch (escola[j].habilitacoes_pretendidas) {	/*criar ligacoes direcionadas do grafo de escola para possiveis professores que poderiam aderir a escola*/
+				case 1 : 
+					for(int k=0; k<100;k++){
+						escola[j].professores_possiveis.push_back(k); /*todos os professores serão possíveis*/
+					}
+					break;
+				case 2:
+					for(int k=0; k<100; k++){
+						if(professor[k].habilitacoes >=2){
+						escola[j].professores_possiveis.push_back(k); /*todos os professores que tem 2 ou 3 habilitacoes*/
+						}
+					}
+					break;
+				case 3:
+					for(int k=0; k<100; k++){
+						if(professor[k].habilitacoes == 3){
+						escola[j].professores_possiveis.push_back(k); /*todos os professores que 3 habilitacoes*/
+						}
+					}
+			}
 			j++;
 		}
-
 	}
 }
+void Inicilizar_Flags(){
+	for(int i=0;i<100;i++){
+		professor[i].flag_professor =0;/*professor nao esta alocado em nenhuma escola ainda*/
+	}
+	for(int i=0;i<50;i++){
+		escola[i].flag_escola =2;/*inicializa a escola com duas vagas*/
+	}
+}
+
+void emparelhamento(){
+	int num_escolaAtual;
+	int num_professor;/* mostra o numero do professor em que esta na lista do vetor das escolas*/
+	vector<int> teacher;
+	vector<int> school;
+
+
+	for(num_escolaAtual=0;num_escolaAtual<50;num_escolaAtual++){ /*fazer um loop para todas as escolas*/
+		school.push_back(num_escolaAtual);
+		school.push_back(num_escolaAtual);/*é colocado duas vezes para facilitar visualizar o match qnd for colocado os professores*/
+
+		vector<int>::iterator aux_vetor = escola[num_escolaAtual].professores_possiveis.begin();
+		while(escola[num_escolaAtual].flag_escola > 0 && aux_vetor != escola[num_escolaAtual].professores_possiveis.end()){ /*ira percorrer todo o vetor de escolas*/ 
+			num_professor =*aux_vetor;															/*e verifica se ja tem os dois professores necessarios*/
+			if(professor[num_professor].flag_professor == 0){
+				vector<int>::iterator it = professor[num_professor].escolas_prioridade.begin();
+				while(it != professor[num_professor].escolas_prioridade.end() ){ /*ira percorrer todo o vetor dos professores para ver se a escola está na lista dele*/
+					if(*it == num_escolaAtual){
+						int aux = *it;
+						teacher.push_back(aux);
+						professor[num_professor].flag_professor = 1;
+						break;/*sai do while atual*/
+					}
+					it++;
+				}		
+			}
+			aux_vetor++;
+		}
+	}
+
+}
 int main(){
-	leitura_arquivo();
+	Fazer_grafo();
+	Inicilizar_Flags();
 }
